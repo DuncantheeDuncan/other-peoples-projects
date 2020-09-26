bb0b2a82c753d0d69f44a571c4f20376809b3c51
DuncantheeDuncan <mthembup69@gmail.com>
2020-09-27 00:05:26 +0200

    front-end done!

--- Arnold-Social-Media-App/Login/ChangePass.php
+++ Arnold-Social-Media-App/Login/ChangePass.php
@@ -1,22 +1,22 @@
 <?php
 require_once("../connection.php");
 if (isset($_POST['username'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $newPass = "UPDATE Users set PassWord ='$password'  WHERE Username ='$username' OR Email='$username'";
-    mysqli_query($conn, $newPass);
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $newPass = "UPDATE Users set PassWord ='$password'  WHERE Username ='$username' OR Email='$username'";
+  mysqli_query($conn, $newPass);
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/style.css"/>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../css/style.css"/>
   <link rel="stylesheet" type="text/css" href="../css/loginStyle.css"/>
-    <title>Change Password</title>
-    
+  <title>Change Password</title>
+  
 </head>
 
 <body>
@@ -24,39 +24,39 @@ if (isset($_POST['username'])) {
     <form method="post">
 
       <div>
-      <label class="label-email">
+        <label class="label-email">
 
-      <input type="text" class="text" name="username" placeholder="username" tabindex="1" required />
-      <span class="required">Username</span>
-    </label>
-  </div>
+          <input type="text" class="text" name="username" placeholder="username" tabindex="1" required />
+          <span class="required">Username</span>
+        </label>
+      </div>
       
-  <div>
-      <label class="label-email">
+      <div>
+        <label class="label-email">
 
-      <input type="password" class="text" name="password" placeholder="username" tabindex="1" required />
-      <span class="required">New Password</span>
-    </label>
-  </div>
+          <input type="password" class="text" name="password" placeholder="username" tabindex="1" required />
+          <span class="required">New Password</span>
+        </label>
+      </div>
 
-<div>
-      <label class="label-email">
+      <div>
+        <label class="label-email">
 
-      <input type="password" class="text" name="password" placeholder="confirm password" tabindex="1" required />
-      <span class="required">Confirm Password</span>
-    </label>
-  </div>
+          <input type="password" class="text" name="password" placeholder="confirm password" tabindex="1" required />
+          <span class="required">Confirm Password</span>
+        </label>
+      </div>
 
-        <input type="submit" value="CHANGE PASSWORD">
-        
-        <div class="email">
-    <a href="index.php">Cancel</a>
+      <input type="submit" value="CHANGE PASSWORD">
+      
+      <div class="email">
+        <a href="index.php">Cancel</a>
 
-  </div>
+      </div>
 
     </form>
     
-    </div>
+  </div>
 </body>
 
 </html>
--- Arnold-Social-Media-App/Login/index.php
+++ Arnold-Social-Media-App/Login/index.php
@@ -56,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     <span></span>
     <span></span>
     <span></span>
-   
+
     <div class="main">
 
       <div class="login_div">
@@ -65,162 +65,112 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
           <h1 class="a11y-hidden">Login Form</h1>
 
           <div>
-            
-             <label class="label-email">
-      <input type="email" class="text" name="email" placeholder="Email" tabindex="1" required />
-      <span class="required">Username/Email</span>
-    </label>
-          </div>
 
-          <input type="checkbox" name="show-password" class="show-password a11y-hidden" id="show-password" tabindex="3" />
-  <label class="label-show-password" for="show-password">
-    <span>Show Password</span>
-  </label>
-  <div>
-    <label class="label-password">
-      <input type="text" class="text" name="password" placeholder="Password" tabindex="2" required />
-      <span class="required">Password</span>
-    </label>
-  </div>
-  <input type="submit" value="SIGN IN" />
-  <input type="submit" class="signup_tab a" value="SIGN UP" />
-   <div class="email">
-    <a href="ChangePass.php">Forgot password?</a>
-
-  </div>
-
-  <figure aria-hidden="true">
-    <div class="person-body"></div>
-    <div class="neck skin"></div>
-    <div class="head skin">
-      <div class="eyes"></div>
-      <div class="mouth"></div>
-    </div>
-    <div class="hair"></div>
-    <div class="ears"></div>
-    <div class="shirt-1"></div>
-    <div class="shirt-2"></div>
-  </figure>
-        </form>
+           <label class="label-email">
+            <input type="email" class="text" name="email" placeholder="Email" tabindex="1" required />
+            <span class="required">Username/Email</span>
+          </label>
         </div>
 
+        <input type="checkbox" name="show-password" class="show-password a11y-hidden" id="show-password" tabindex="3" />
+        <label class="label-show-password" for="show-password">
+          <span>Show Password</span>
+        </label>
+        <div>
+          <label class="label-password">
+            <input type="text" class="text" name="password" placeholder="Password" tabindex="2" required />
+            <span class="required">Password</span>
+          </label>
+        </div>
+        <input type="submit" value="SIGN IN" />
+        <input type="submit" class="signup_tab a" value="SIGN UP" />
+        <div class="email">
+          <a href="ChangePass.php">Forgot password?</a>
 
+        </div>
 
-      <div class="register_div" hidden>
-        
-        <form method="post" id="login-form" action="" class="login-form" autocomplete="off" role="main">
-
-     <div>
-      <label class="label-email">
-
-      <input type="text" class="text" name="name" placeholder="name" tabindex="1" required />
-      <span class="required">Name</span>
-    </label>
-  </div>
-
-
-<div>
-      <label class="label-email">
-
-      <input type="text" class="text" name="surname" placeholder="Surname" tabindex="1" required />
-      <span class="required">Surname</span>
-    </label>
-  </div>
-
-          
-
-<div>
-      <label class="label-email">
-
-      <input type="text" class="text" name="username" placeholder="Username" tabindex="1" required />
-      <span class="required">Username</span>
-    </label>
-  </div>
-
-
-        
-
-<div>
-      <label class="label-email">
-
-      <input type="text" class="text" name="newEmail" placeholder="Email Adress" tabindex="1" required />
-      <span class="required">Email Adress</span>
-    </label>
-  </div>
-          
-
-<div>
-      <label class="label-email">
-
-      <input type="text" class="text" name="password" placeholder="Password" tabindex="1" required />
-      <span class="required">Password</span>
-    </label>
-  </div>
-
-          
-          <div>
-      <label class="label-email">
-
-      <input type="password" class="text" name="confirm_password" placeholder="Confirm Password" tabindex="1" required />
-      <span class="required">Confirm Password</span>
-    </label>
-  </div>
-
-        
-          
-            <input type="submit" value="SIGN UP">
-          
-
-
-        </form>
-
-      </div>
-    
-  </div>
-  <script type="text/javascript" src="../JS/jquery-1.3.2.min.js"></script>
-  <script type="text/javascript" src="../JS/Main.js"></script>
-</body>
-
-
-
-
-
-<!-- <form method="get" action="javascript: void(0);" id="login-form" class="login-form" autocomplete="off" role="main"> -->
-  <!-- <h1 class="a11y-hidden">Login Form</h1> -->
-  <!-- <div>
-    <label class="label-email">
-      <input type="email" class="text" name="email" placeholder="Email" tabindex="1" required />
-      <span class="required">Email</span>
-    </label>
-  </div> -->
-
-  <!-- <input type="checkbox" name="show-password" class="show-password a11y-hidden" id="show-password" tabindex="3" /> -->
-  <!-- <label class="label-show-password" for="show-password"> -->
-    <!-- <span>Show Password</span> -->
-  <!-- </label> -->
-  <!-- <div> -->
-    <!-- <label class="label-password"> -->
-      <!-- <input type="text" class="text" name="password" placeholder="Password" tabindex="2" required /> -->
-      <!-- <span class="required">Password</span> -->
-    <!-- </label> -->
-  <!-- </div> -->
-
-  <!-- <input type="submit" value="Log In" /> -->
-  <!-- <div class="email"> -->
-    <!-- <a href="#">Forgot password?</a> -->
-  <!-- </div> -->
-
- <!--  <figure aria-hidden="true">
-    <div class="person-body"></div>
-    <div class="neck skin"></div>
-    <div class="head skin">
-      <div class="eyes"></div>
-      <div class="mouth"></div>
+        <figure aria-hidden="true">
+          <div class="person-body"></div>
+          <div class="neck skin"></div>
+          <div class="head skin">
+            <div class="eyes"></div>
+            <div class="mouth"></div>
+          </div>
+          <div class="hair"></div>
+          <div class="ears"></div>
+          <div class="shirt-1"></div>
+          <div class="shirt-2"></div>
+        </figure>
+      </form>
     </div>
-    <div class="hair"></div>
-    <div class="ears"></div>
-    <div class="shirt-1"></div>
-    <div class="shirt-2"></div>
-  </figure> -->
-<!-- </form> -->
+
+
+
+    <div class="register_div" hidden>
+
+      <form method="post" id="login-form" action="" class="login-form" autocomplete="off" role="main">
+
+       <div>
+        <label class="label-email">
+
+          <input type="text" class="text" name="name" placeholder="name" tabindex="1" required />
+          <span class="required">Name</span>
+        </label>
+      </div>
+
+
+      <div>
+        <label class="label-email">
+
+          <input type="text" class="text" name="surname" placeholder="Surname" tabindex="1" required />
+          <span class="required">Surname</span>
+        </label>
+      </div>
+
+
+
+      <div>
+        <label class="label-email">
+
+          <input type="text" class="text" name="username" placeholder="Username" tabindex="1" required />
+          <span class="required">Username</span>
+        </label>
+      </div>
+
+      <div>
+        <label class="label-email">
+
+          <input type="text" class="text" name="newEmail" placeholder="Email Adress" tabindex="1" required />
+          <span class="required">Email Adress</span>
+        </label>
+      </div>
+
+
+      <div>
+        <label class="label-email">
+
+          <input type="text" class="text" name="password" placeholder="Password" tabindex="1" required />
+          <span class="required">Password</span>
+        </label>
+      </div>
+
+
+      <div>
+        <label class="label-email">
+
+          <input type="password" class="text" name="confirm_password" placeholder="Confirm Password" tabindex="1" required />
+          <span class="required">Confirm Password</span>
+        </label>
+      </div>
+
+      <input type="submit" value="SIGN UP">
+
+    </form>
+
+  </div>
+
+</div>
+<script type="text/javascript" src="../JS/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="../JS/Main.js"></script>
+</body>
 </html>
