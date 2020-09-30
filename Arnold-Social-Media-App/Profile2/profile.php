d9834064611705415048aef724c0e9599639eba0
DuncantheeDuncan <mthembup69@gmail.com>
2020-09-30 21:59:01 +0200

    prifile like added

---
+++ Arnold-Social-Media-App/Profile2/profile.php
@@ -0,0 +1,206 @@
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	<script src="jquery.js"></script>
+	<script>$(document).ready(function(){
+
+		$(".box13").click(function(){
+			$(".box13").css('background-color','#e9f0f2');
+			$(".box11").css('background-color','white');
+			$(".box12").css('background-color','white');
+			$(".box14").css('background-color','white');
+			$(".frnpad").fadeIn("slow");
+			$(".frnpadx").fadeIn("slow");
+			$(".aboutpad").fadeOut("slow");
+			$(".aboutpadx").fadeOut("slow");
+			$(".phpad").fadeOut("slow");
+			$(".phpadx").fadeOut("slow");
+			$(".postp").fadeOut("slow");
+			$(".postpx").fadeOut("slow");
+			$(".post1p").fadeOut("slow");
+			$(".post1px").fadeOut("slow");
+			$(".abox1").fadeOut("slow");
+			$(".abox1x").fadeOut("slow");
+			$(".abox2").fadeOut("slow");
+			$(".abox2x").fadeOut("slow");
+			$(".abox3").fadeOut("slow");
+			$(".abox3x").fadeOut("slow");
+		});
+
+		$(".box14").click(function(){
+			$(".box14").css('background-color','#e9f0f2');
+			$(".box11").css('background-color','white');
+			$(".box12").css('background-color','white');
+			$(".box13").css('background-color','white');
+			$(".phpad").fadeIn("slow");
+			$(".phpadx").fadeIn("slow");
+			$(".aboutpad").fadeOut("slow");
+			$(".aboutpadx").fadeOut("slow");
+			$(".frnpad").fadeOut("slow");
+			$(".frnpadx").fadeOut("slow");
+			$(".postp").fadeOut("slow");
+			$(".postpx").fadeOut("slow");
+			$(".post1p").fadeOut("slow");
+			$(".post1px").fadeOut("slow");
+			$(".abox1").fadeOut("slow");
+			$(".abox1x").fadeOut("slow");
+			$(".abox2").fadeOut("slow");
+			$(".abox2x").fadeOut("slow");
+			$(".abox3").fadeOut("slow");
+			$(".abox3x").fadeOut("slow");
+		});
+
+		$(".box12").click(function(){
+			$(".box12").css('background-color','#e9f0f2');
+			$(".box11").css('background-color','white');
+			$(".box13").css('background-color','white');
+			$(".box14").css('background-color','white');
+			$(".aboutpad").fadeIn("slow");
+			$(".aboutpadx").fadeIn("slow");
+			$(".frnpad").fadeOut("slow");
+			$(".frnpadx").fadeOut("slow");
+			$(".phpad").fadeOut("slow");
+			$(".phpadx").fadeOut("slow");
+			$(".postp").fadeOut("slow");
+			$(".postpx").fadeOut("slow");
+			$(".post1p").fadeOut("slow");
+			$(".post1px").fadeOut("slow");
+			$(".abox1").fadeOut("slow");
+			$(".abox1x").fadeOut("slow");
+			$(".abox2").fadeOut("slow");
+			$(".abox2x").fadeOut("slow");
+			$(".abox3").fadeOut("slow");
+			$(".abox3x").fadeOut("slow");
+		});
+
+		$(".box11").click(function(){
+			$(".box11").css('background-color','#e9f0f2');
+			$(".box12").css('background-color','white');
+			$(".box13").css('background-color','white');
+			$(".box14").css('background-color','white');
+			$(".aboutpad").fadeOut("slow");
+			$(".aboutpadx").fadeOut("slow");
+			$(".postp").fadeIn("slow");
+			$(".postpx").fadeIn("slow");
+			$(".post1p").fadeIn("slow");
+			$(".post1px").fadeIn("slow");
+			$(".abox1").fadeIn("slow");
+			$(".abox1x").fadeIn("slow");
+			$(".abox2").fadeIn("slow");
+			$(".abox2x").fadeIn("slow");
+			$(".abox3").fadeIn("slow");
+			$(".abox3x").fadeIn("slow");
+			$(".frnpad").fadeOut("slow");
+			$(".frnpadx").fadeOut("slow");
+			$(".phpad").fadeOut("slow");
+			$(".phpadx").fadeOut("slow");
+
+		});
+		$(".box13").click(function(){})
+
+
+	});
+</script>
+
+</head>
+<body>
+
+
+	<div class="header1">
+		<div id="img3" class="header1"><img src="chat.png" id="img3"/></div>
+
+		<div id="searcharea" class="header1"><input placeholder="search here..." type="text" id="searchbox"/></div>
+		<div id="profilearea4" class="header1"><a href="profile.php">Home</a></div>
+		
+	</div>
+
+	<div class="header0001">
+	</div>
+
+	<div class="coverpad">
+	</div>
+	<div class="coverpadx"><img src="cover.jpg" width="850px"/>
+	</div>
+
+
+
+	<div class="profilepic">
+	</div>
+	<div class="profilepicx"><img src="picture.jpg" height="140px"/>
+	</div>
+	<div class="username">chioma <br>Mumbaiwa
+	</div>
+	<div class="box11">Timeline
+	</div>
+	<div class="box12">About
+	</div>
+	<div class="box13">Friends
+	</div>
+	<div class="box14">Photos
+	</div>
+
+
+	<select>
+		<option selected>More</option>
+		<option value="saab">Videos</option>
+		<option value="opel">Places</option>
+		<option value="audi">Pages</option>
+	</select>
+
+
+	<div class="postp">
+		<div id="column-1" class="postp">update status | add photos/videos | create photo album<hr><br><br><br><br><br><br><hr></div>
+		<div id="postpos" class="postp"><input type="submit" id="buttonpost" value="post"/></div>
+		<div id="postboxpos" class="postp"><textarea placeholder="What's in your mind" id="postbox" type="text"></textarea></div>
+
+	</div>
+	<div class="postpx">
+	</div>
+
+</div>
+<div class="post1px">
+</div>
+<div class="post1p"><img src="picture.jpg"height="30"/><br><br><img src="picture.jpg" height="411" width="580"/><br><br><p6>Like  Comment  Share</p6><br><hr><p1>Arnold</p1><p2> and</p2><p1> 5 others</p1><p2> like this</p2>
+	<div id="post2text" class="post1p"><p3>Chioma </p3><p2>with </p2><p1> Arup Pegu</p1><p2> and</p2><p1> 15 others.</p1><br><p4>4 hrs.</p4></div>
+	<div id="commentprof2" class="post1p"><img src="picture.jpg"height="30"/></div>
+	<div id="commentboxpos2" class="post1p"><input type="textarea" placeholder="comment" id="commentbox"/></div>
+</div>
+</div>
+<div class="aboutpad">about
+</div>
+<div class="aboutpadx">
+</div>
+<div class="abox1">
+</div>
+
+<div class="abox1x">
+</div>
+
+<div class="abox2">
+
+</div>
+
+<div class="abox2x">
+</div>
+
+<div class="abox3">
+</div>
+
+<div class="abox3x">
+</div>
+
+
+<div class="frnpadx">
+	friends
+</div>
+<div class="phpad">photo
+
+	MYBE TRY ONE OF THE GALLARY-ISH for PHOTOS
+</div>
+
+<div class="phpadx">
+</div>
+
+
+</body>
+</html>
